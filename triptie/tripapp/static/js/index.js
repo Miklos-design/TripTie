e70d6eb5cc239204b2363c4a6da7a3d24ffda6b8
Miki-design <73119146+Miklos-design@users.noreply.github.com>
2024-03-12 19:32:06 +0000

    new login

--- triptie/tripapp/static/js/index.js
+++ triptie/tripapp/static/js/index.js
@@ -1,10 +1,12 @@
 const menuToggle = document.querySelector('.toggle');
 const showcase = document.querySelector('.showcase');
+const loginRegisterLink = document.querySelector('.menu ul li:nth-child(3) a');
 
 menuToggle.addEventListener('click', () => {
   menuToggle.classList.toggle('active');
   showcase.classList.toggle('active');
 });
+
 function tweetShare() {
   window.open('https://twitter.com/intent/tweet?text=Check%20out%20this%20awesome%20website%21%20https%3A%2F%2Fexample.com%2F');
 }
@@ -16,9 +18,21 @@ function shareOnFacebook() {
 
 function openForm(formName) {
   document.getElementById(formName).style.display = "block";
+
 }
 
-function closeForm() {
-  document.getElementById("loginForm").style.display = "none";
-  document.getElementById("registerForm").style.display = "none";
+function closeForm(loginForm) {
+  document.getElementById(loginForm).style.display = "none";
+}
+
+function openRegisterForm() {
+  // Close the login form if it's open
+  closeForm("loginForm");
+  // Open the register form
+  openForm("registerForm");
+}
+
+function closeRegisterForm() {
+  // Close the register form
+  closeForm("registerForm");
 }
